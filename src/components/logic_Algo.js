fff39379d5d53449ee7b906d72eddc2709cc0e50
PranayBandaru <pranay18539@mechyd.ac.in>
2020-12-05 13:58:54 +0530

    Numbers tab complete
    Numbers tab completed

---
+++ src/components/logic_Algo.js
@@ -0,0 +1,303 @@
+var input  = [ { product : "XPS" , backlog : 15 , sales: 12 , extra : 20}, { product : "Vostro" , backlog : 10 , sales: 16 , extra : 12},
+{ product : "GSeries" , backlog : 4 , sales: 15 , extra : 2}, { product : "Inspiron" , backlog : 9 , sales: 10 , extra : 17}]
+var times = {
+    XPS:[2,3,2],    //XPS
+    Inspiron: [2,2,2],    //Inspiron
+    Vostro: [1,2,1],    //Vostro
+    GSeries: [3,3,1]     //GSeries
+};
+
+var n_lines = 6;
+
+const initialise = (arr)=> {
+    for(var i=0 ; i<336;i++){
+        arr[i] = 0;
+    }
+    return arr; 
+}
+
+input.sort((a,b)=>{
+    return a.backlog - b.backlog 
+})
+
+//console.log(input)
+
+var line_1 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+var line_2 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+var line_3 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+var line_4 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+var line_5 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+var line_6 = {step1 : initialise(new Array(336)),step2 : initialise(new Array(336)), step3 : initialise(new Array(336)) }
+
+var lines = [line_1, line_2, line_3, line_4, line_5, line_6];
+
+// console.log(line_4.step2[50]);
+var backlogs = [];
+var sales = [];
+var extra = [];
+
+//-----------------------------------------------FIRST INDEX--------------------------------------------------
+function first_index(lines, step_no,min){
+    //console.log(lines);
+    var min_index = 336;
+    var min_line_no;
+    step_no = String("step"+step_no);
+    for(var i = 0; i<lines.length; i++){
+        for(var k =min; k<336; k++){
+            if(lines[i][step_no][k] == 0 && k < min_index){
+                min_index = k;
+                min_line_no = i+1;
+                break;
+            }
+        }
+    }
+    return([min_index, min_line_no]);
+}
+
+//-----------------------------------------------OPTIMIZE--------------------------------------------------
+export const optimize = (input) => {
+    var temp_swap;
+    var loc;
+    for(var i=0;i<4;i++){
+        backlogs.push({product :input[i].product , backlog:  input[i].backlog})
+    }for(var i=0;i<4;i++){
+        sales.push({product :input[i].product , sales:  input[i].sales })
+    }for(var i=0;i<4;i++){
+        extra.push({product :input[i].product , extra:  input[i].extra })
+    }
+    //console.log(backlogs);
+    //----------------------------------------------SORT----------------------------------------------------------
+    for(var i =0; i<3;i++){
+        for(var j = 0; j<3;j++){
+            if(backlogs[j].backlog < backlogs[j+1].backlog){
+                temp_swap = backlogs[j].backlog;
+                backlogs[j].backlog = backlogs[j+1].backlog;
+                backlogs[j+1].backlog = temp_swap;
+
+                temp_swap = backlogs[j].product;
+                backlogs[j].product = backlogs[j+1].product;
+                backlogs[j+1].product = temp_swap;
+            }
+        }
+    }
+
+    for(var i =0; i<3;i++){
+        for(var j = 0; j<3;j++){
+            if(sales[j].sales < sales[j+1].sales){
+                temp_swap = sales[j].sales;
+                sales[j].sales = sales[j+1].sales;
+                sales[j+1].sales = temp_swap;
+
+                temp_swap = sales[j].product;
+                sales[j].product = sales[j+1].product;
+                sales[j+1].product = temp_swap;
+            }
+        }
+    }
+
+    for(var i =0; i<3;i++){
+        for(var j = 0; j<3;j++){
+            if(extra[j].extra < extra[j+1].extra){
+                temp_swap = extra[j].extra;
+                extra[j].extra = extra[j+1].extra;
+                extra[j+1].extra = temp_swap;
+
+                temp_swap = extra[j].product;
+                extra[j].product = extra[j+1].product;
+                extra[j+1].product = temp_swap;
+            }
+        }
+    }
+    //----------------------------------------------SORT END----------------------------------------------------------
+    //console.log(backlogs);
+    //console.log(times[p_name][0]);
+    var step_complete = {step1: 0,step2: 0,step3: 0};
+    var step_ends = {step1: 0,step2: 0,step3: 0};
+    //---------------------------------------------BACKLOGS-----------------------------------------------------------
+    for(var lv = 0; lv<4; lv++){
+        var p1_backlogs = backlogs[lv].backlog;
+        var p_name = backlogs[lv].product;
+        for(var i = 0; i<(backlogs[lv].backlog); i++){
+            if(p1_backlogs>0){
+                loc = first_index(lines,1,0);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][0]; j++){
+                    lines[loc[1]-1].step1[loc[0]+j]  = backlogs[lv].product;
+                    step_ends.step1 =loc[0]+j+1;
+                }
+                p1_backlogs--;
+                step_complete.step1++;
+            }
+            if(step_complete.step1>0){
+                loc = first_index(lines,2,step_ends.step1);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][1]; j++){
+                    lines[loc[1]-1].step2[loc[0]+j]  = backlogs[lv].product;
+                    step_ends.step2 = loc[0]+j+1;
+                }
+                step_complete.step1--;
+                step_complete.step2++;
+            }
+            if(step_complete.step2>0){
+                loc = first_index(lines,3,step_ends.step2);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][2]; j++){
+                    lines[loc[1]-1].step3[loc[0]+j]  = backlogs[lv].product;
+                    step_ends.step3 = loc[0]+j+1;
+                }
+                step_complete.step2--;
+                step_complete.step3++;
+                //console.log(lines[loc[1]].step1[loc[0]] );
+            }
+        }
+    }   
+    //---------------------------------------------SALES-----------------------------------------------------------
+    
+    for(var lv = 0; lv<4; lv++){
+        var p1_sales = sales[lv].sales;
+        var p_name = sales[lv].product;
+        for(var i = 0; i<(sales[lv].sales); i++){
+
+            if(p1_sales>0){
+                loc = first_index(lines,1,0);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][0]; j++){
+                    lines[loc[1]-1].step1[loc[0]+j]  = sales[lv].product;
+                }
+                p1_sales--;
+                step_complete.step1++;
+            }
+            if(step_complete.step1>0){
+                loc = first_index(lines,2,step_ends.step1);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][1]; j++){
+                    //console.log(loc);
+                    //console.log(step_ends.step2);
+                    lines[loc[1]-1].step2[loc[0]+j]  = sales[lv].product;
+                }
+                step_complete.step1--;
+                step_complete.step2++;
+            }
+            if(step_complete.step2>0){
+                loc = first_index(lines,3,step_ends.step2);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][2]; j++){
+                    lines[loc[1]-1].step3[loc[0]+j]  = sales[lv].product;
+                }
+                step_complete.step2--;
+                step_complete.step3++;
+                //console.log(lines[loc[1]].step1[loc[0]] );
+            }
+        }
+    }
+    
+    //---------------------------------------------------EXTRA-----------------------------------------------------------
+    for(var lv = 0; lv<4; lv++){
+        var p1_extra = extra[lv].extra;
+        var p_name = extra[lv].product;
+        for(var i = 0; i<(extra[lv].extra); i++){
+            if(p1_extra>0){
+                loc = first_index(lines,1,0);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][0]; j++){
+                    lines[loc[1]-1].step1[loc[0]+j]  = extra[lv].product;
+                    step_ends.step1 =loc[0]+j+1;
+                }
+                p1_extra--;
+                step_complete.step1++;
+            }
+            if(step_complete.step1>0){
+                loc = first_index(lines,2,step_ends.step1);
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][1]; j++){
+                    lines[loc[1]-1].step2[loc[0]+j]  = extra[lv].product;
+                    step_ends.step2 = loc[0]+j+1;
+                }
+                step_complete.step1--;
+                step_complete.step2++;
+            }
+            if(step_complete.step2>0){
+                loc = first_index(lines,3,step_ends.step2);
+
+                for(var t = 0; t<times[p_name][0]; t++){
+                    if((loc[0]+t+1)%16 == 0){
+                        loc[0] = loc[0] + t+1;
+                    }
+                }
+                for(var j =0; j< times[p_name][2]; j++){
+                    lines[loc[1]-1].step3[loc[0]+j]  = extra[lv].product;
+                    step_ends.step3 = loc[0]+j+1;
+                }
+                step_complete.step2--;
+                step_complete.step3++;
+                //console.log(lines[loc[1]].step1[loc[0]] );
+            }
+        }
+    }   
+    
+    
+        
+    //step_complete_temp = (step_complete.step1/6+1)*times[p_name][0]; 
+
+    /*
+    console.log(step_complete);
+    for(var i = 0; i< step_complete_temp; i++){
+        for(var j = 0; j<6 && step_complete.step1>0; j++){
+            
+            for(var k =0; k<times[p_name][0]; k++){
+                lines[j].step1[i+k] = backlogs[0].product ;
+            }
+            step_complete.step1--;
+            step_complete.step2++;
+        }
+    }
+    */
+   
+//step_complete_temp = (step_complete.step1/6+1)*times[p_name][0]; 
+
+
+    console.log(lines);
+    //console.log(lines[1].step1[250])
+    //console.log(first_index(lines,1));
+    return(lines);
+
+
+
+}
+
+optimize(input) 
+
+
+// bubble_Sort(backlogs)
--- src/components/numbers.js
+++ src/components/numbers.js
@@ -1,23 +1,53 @@
 import React from "react";
-
+import {optimize} from "./logic_Algo.js";
 var button_name = "N E X T";
 
 
 export class numbers extends React.Component {
 
-    constructor(){
-        super();
+
+    
+    constructor(props){
+        super(props);
         this.toggleClass1 = this.toggleClass1.bind(this);
         this.toggleClass2 = this.toggleClass2.bind(this);
         this.toggleClass3 = this.toggleClass3.bind(this);
         this.toggleClass4 = this.toggleClass4.bind(this);
         this.b_click = this.b_click.bind(this);
+        this.check_empty = this.check_empty.bind(this);
         this.state = {
             active1: true,
             active2: false,
             active3: false,
             active4: false,
-        };
+            pred_num: null,
+            current_ord_num: null,
+            backlog_num: null,
+            final :[{ product : "XPS" , backlog : 0 , sales: 0 , extra : 0},
+            { product : "Vostro" , backlog : 0 , sales: 0 , extra : 0},
+            { product : "GSeries" , backlog : 0 , sales: 0 , extra : 0},
+            { product : "Inspiron" , backlog : 0 , sales: 0 , extra : 0}]
+          };
+    }
+
+    onChangeOfInput = (name) =>{
+      console.log(name);
+      console.log("value");
+      var val = document.getElementById(name).value;
+      console.log(val);
+      this.setState({
+        [name] : val
+      });
+      console.log(this.state);
+    };
+
+    check_empty(){
+      if(this.state.pred_num == null || this.state.backlog_num == null || this.state.current_ord_num == null){
+        return false;
+      }
+      else{
+        return true;
+      }
     }
 
     b_click(){
@@ -26,25 +56,61 @@ export class numbers extends React.Component {
       const currentState2 = this.state.active2;
       const currentState3 = this.state.active3;
       const currentState4 = this.state.active4;
-      console.log(this.state);
-      if(currentState1){
-        console.log("S1 hit");
-        this.setState({ active1:false });
-        this.setState({ active2: true });
-      }else if(currentState2){
-        console.log("S2 hit");
-        this.setState({ active2:false });
-        this.setState({ active3: true });
-      }else if(currentState3){
-        console.log("S3 hit");
-        this.setState({ active3:false });
-        this.setState({ active4: true });
+      //console.log(this.state);
+      
+      if(this.check_empty() == false){
+        document.getElementById("warning_label").style.color = "#d10d1a";
       }
-      else if(currentState4){
-        console.log("S4 hit");
-        //GO FOR PROCESSING
+      else{        
+        document.getElementById("warning_label").style.color = "#252525";
+        if(currentState1){
+          console.log("S1 hit");
+          this.setState({ active1:false });
+          this.setState({ active2: true });
+          this.state.final[0].backlog = parseInt(this.state.backlog_num);
+          this.state.final[0].sales = parseInt(this.state.current_ord_num);
+          this.state.final[0].extra = this.state.pred_num - this.state.current_ord_num;        
+          console.log(this.state.final);  
+        }
+        else if(currentState2){
+          console.log("S2 hit");
+          this.setState({ active2:false });
+          this.setState({ active3: true });
+          this.state.final[1].backlog = parseInt(this.state.backlog_num);
+          this.state.final[1].sales = parseInt(this.state.current_ord_num);
+          this.state.final[1].extra = this.state.pred_num - this.state.current_ord_num;
+          console.log(this.state.final);
+        }
+        else if(currentState3){
+          console.log("S3 hit");
+          this.setState({ active3:false });
+          this.setState({ active4: true });
+          this.state.final[2].backlog = parseInt(this.state.backlog_num);
+          this.state.final[2].sales = parseInt(this.state.current_ord_num);
+          this.state.final[2].extra = this.state.pred_num - this.state.current_ord_num;
+          console.log(this.state.final);
+        }
+        else if(currentState4){
+          console.log("S4 hit");
+          this.state.final[3].backlog = parseInt(this.state.backlog_num);
+          this.state.final[3].sales = parseInt(this.state.current_ord_num);
+          this.state.final[3].extra = this.state.pred_num - this.state.current_ord_num;
+          console.log(this.state.final);
+
+          //GO FOR PROCESSING
+          var lines = optimize(this.state.final);
+          window.scrollBy(0,700);
+
+        }
+        console.log(this.state);
+        this.setState({pred_num: null});
+        this.setState({backlog_num: null});
+        this.setState({current_ord_num: null});
+
+        document.getElementById("pred_num").value = 0;
+        document.getElementById("backlog_num").value = 0;
+        document.getElementById("current_ord_num").value = 0;
       }
-      console.log(this.state);
     }
     
 
@@ -77,6 +143,7 @@ export class numbers extends React.Component {
     this.setState({ active3: !currentState });
     this.setState({ active4: false });
 
+
     console.log(this.state);
 };
 
@@ -92,6 +159,7 @@ toggleClass4() {
 
 
   render() {
+    
     return (
       <div id="numbers" className="text-center">
         <div className="container">
@@ -113,9 +181,9 @@ toggleClass4() {
                 </ul>
           </div>
 
-          <input type="number" id="pred_num" name="pred_num"></input>
-          <input type="number" id="backlog_num" name="backlog_num"></input>
-          <input type="number" id="current_ord_num" name="current_ord_num"></input>
+          <input type="number" id="pred_num" name="pred_num" placeholder="0" onChange={this.onChangeOfInput.bind(this, "pred_num")}></input>
+          <input type="number" id="backlog_num" name="backlog_num"  placeholder="0" onChange={this.onChangeOfInput.bind(this , "backlog_num")}></input>
+          <input type="number" id="current_ord_num" name="current_ord_num"  placeholder="0" onChange={this.onChangeOfInput.bind(this, "current_ord_num")}></input>
           <a id = "pred_label_a">
               PREDICTED SALES FOR THE WEEK
           </a>        
@@ -125,10 +193,12 @@ toggleClass4() {
           <a id = "current_label_a">
               CURRENT ORDERS
           </a>
-
+          <h3 id = "warning_label">
+            One or more values are empty
+          </h3>
           <button id = "compute_button" 
-                    className="btn btn-custom btn-lg page-scroll" onClick={this.b_click.bind(this)}> {this.state.active4 ? "C O M P U T E" : "N E X T"} 
-                    </button>
+                  className="btn btn-custom btn-lg page-scroll" onClick={this.b_click.bind(this)}> {this.state.active4 ? "C O M P U T E" : "N E X T"} 
+                  </button>
           <div className="row">
               
           </div>
